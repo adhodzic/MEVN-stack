cfb429b71f052f78839853094015e930f12bd4ce
adhodzic <adhodzic@student.unipu.hr>
2022-01-27 21:50:34 +0100

    client

---
+++ client/src/views/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="profile">
+        <div class="card">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Username</label>
+                    <input v-model="username" type="username" class="form-control" id="exampleInputUsername1" aria-describedby="usernameHelp" placeholder="Username" />
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+                <button @click="loginUser" class="btn btn-primary">Login</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default({
+  data(){
+    return{
+      username: "",
+      password: ""
+    }
+  }
+})
+</script>
+
---
+++ client/src/views/Register.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="register">
+        <div class="card">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Username</label>
+                    <input v-model="username" type="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username" />
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+                <button @click="registerUser" class="btn btn-primary">Register</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        async registerUser() {
+            if (this.username == "" || this.password.length < 6) {
+                alert("email should not be empty and password must contain at least 6 characters");
+                return;
+            }
+
+            try {
+                const user = {
+                    username: this.username,
+                    password: this.password,
+                };
+                const response = await axios.post("http://localhost:5000/api/register", user);
+                this.$store.commit('setToken', response.data)
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
+
+<style>
+label {
+    font-size: 20px;
+}
+.register {
+    display: flex;
+    justify-content: center;
+    padding: 100px 0 0 0;
+}
+.card {
+    width: 400px;
+}
+</style>
